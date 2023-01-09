183e1c44b992906d58ba17e4b200b1466c9ae637
Keith Taylor <taylor.km61@icloud.com>
2023-01-09 12:26:17 +0000

    Middleware

--- carsharing.py
+++ carsharing.py
@@ -26,5 +26,14 @@ async def unicorn_exception_handler(request: Request, exc: BadTripException):
         content={"message": "Bad Trip"},
     )
 
+
+# Example of Middleware, set cookie whenever a request comes in
+@app.middleware("http")
+async def add_cars_next(request: Request, call_next):
+    response = await call_next(request)
+    response.set_cookie(key="cars_cookie", value="you_visited_the_carsharing_app")
+    return response
+
+
 if __name__ == "__main__":
     uvicorn.run("carsharing:app", reload=True)
--- routers/web.py
+++ routers/web.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, Form, Depends
+from fastapi import APIRouter, Request, Depends, Form, Cookie
 from sqlmodel import Session
 from starlette.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
@@ -12,18 +12,20 @@ templates = Jinja2Templates(directory="templates")
 
 
 @router.get("/", response_class=HTMLResponse)
-def home(request: Request):
+def home(request: Request, cars_cookie: str | None = Cookie(None)):
+    # Has the agent visited page before?
+    print(cars_cookie)
+    # We have to pass a dict containing the request, otherwise FASTApi will give an error
     return templates.TemplateResponse("home.html", {"request": request})
 
 
-# Method signature has '*' to everything after is a kwarg. This gets around putting arguments without a default
-# after arguments with a default. Normally python rules that arguments without default should come first.  It is
-# common in fastapi
-# The Form(...) tells fastapi to handle the form data as required (but we can put a value for default value)
-# The tree dots are python class 'ellipsis', but python doesn't specify what it means.
-# Call the get_cars function that we already have to get the cars.  In this case fastapi does not inject the session
-# when we call get_cars like this since it is not called as a fastapi operation, so we pass session to it.
+# Arguments without a default in python have to come first, but we like fastapi objects to come first
+# and stuff that the user inputs to come later, so that is why the parameter list starts with *.
+# It turns everything that comes after the * into a keyword argument. Now order doesn't matter.
+# The three dots inside Form means required field, ... belongs to python elipsis class which has no real meaning.
 @router.post("/search", response_class=HTMLResponse)
 def search(*, size: str = Form(...), doors: int = Form(...), request: Request, session: Session = Depends(get_session)):
+    # We call our already coded get_cars, but we do not get the benefit of DI,
+    # so have to pass session on to the function
     cars = get_cars(size=size, doors=doors, session=session)
     return templates.TemplateResponse("search_results.html", {"request": request, "cars": cars})
